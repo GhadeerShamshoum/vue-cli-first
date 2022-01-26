9e7dc0d934307674af61905a14b5d21ed0a6100d
Ghadeer <ghaora@gmail.com>
2022-01-26 21:07:04 +0200

    adding text to bonus

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -12,6 +12,10 @@
       </div>
        <div class="photoContainer">
         <img src="https://i.pinimg.com/originals/fd/38/d0/fd38d0c436dec6077fe341b74f05c2d7.jpg" alt="">
+      <div class="cover">
+        <div class="marvel"><strong>MARVEL</strong></div>
+        <button>Watch now!</button>
+      </div>
       </div>
 
     </div>
@@ -25,16 +29,24 @@ export default {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 .jumbotorn{
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 100%;
 
 }
 .container {
-  height: 300px;
-  width: 500px;
+  height: 400px;
+  width: 60%;
   display: flex;
+  position: relative;
+  
 }
 
 .photoContainer{
@@ -48,5 +60,35 @@ img{
   height: 100%;
 
 }
+
+.cover{
+  background-color: rgba(0, 0, 0, 0.281);
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.marvel{
+  color:white;
+  font-size: 75px;
+  background-color: red;
+  padding: 30px;
+  width: 400px;
+}
+
+button{
+  border-radius: 20px;
+  padding: 10px;
+  margin: 20px;
+  background-color: gray;
+}
+
+
 
 </style>
