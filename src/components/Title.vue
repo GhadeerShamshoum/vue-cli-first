31f73c7f6462d8e75322c8751a6bc0551f4c3528
Ghadeer <ghaora@gmail.com>
2022-01-26 16:34:29 +0200

    first commit

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="hello">
-    
+  <div>
+    <h3>La mia prima app con Vue CLI</h3> 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'Title'
 }
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
